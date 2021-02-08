d470997b55f0409f6bb4196da8fe8a11c78a9c39
S460 <aleksandar.ta6ev@gmail.com>
2021-02-08 12:14:35 +0200

    Edits can be canceled

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,57 @@
-function Task(props) {
-	const { taskObj, num, delTask, editTask } = props;
+import React from 'react';
+class Task extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			editOn: false,
+		};
+		this.handleEdit = this.handleEdit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSave = this.handleSave.bind(this);
+	}
 
-	return (
-		<div className='taskCard'>
-			<h3>
-				{num}: {taskObj.task}
-			</h3>
-			<button onClick={() => delTask(taskObj.key)}>Delete Task</button>
-			<button
-				onClick={() => {
-					let newValue = prompt('New task name', taskObj.task);
-					editTask(taskObj.key, newValue);
-				}}>
-				Edit Task
-			</button>
-		</div>
-	);
+	handleChange(e) {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+	}
+
+	handleEdit() {
+		this.setState((prevState) => {
+			return { editOn: !prevState.editOn };
+		});
+	}
+
+	handleSave(e) {
+		this.handleEdit();
+		this.props.editTask();
+	}
+
+	render() {
+		const { taskObj, num, delTask } = this.props;
+
+		if (this.state.editOn) {
+			return (
+				<div className='taskCard'>
+					<input
+						type='text'
+						placeholder='New task name'
+						value={this.props.taskObj.task}
+					/>
+					<button onClick={this.handleEdit}>Cancel</button>
+					<button onClick={this.handleClick}>Submit</button>
+				</div>
+			);
+		}
+		return (
+			<div className='taskCard'>
+				<h3>
+					{num}: {taskObj.task}
+				</h3>
+				<button onClick={() => delTask(taskObj.key)}>Delete Task</button>
+				<button onClick={this.handleEdit}>Edit Task</button>
+			</div>
+		);
+	}
 }
 
 export default Task;
